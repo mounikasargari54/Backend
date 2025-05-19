1014420938211df4a610e1f8d2c04f9ef25544b3
mounikasargari54 <mounikareddysargari@gmail.com>
2025-05-19 16:58:31 +0530

    add user and video model

---
+++ src/models/user.model.js
@@ -0,0 +1,83 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // URL to the avatar image it is cloudnary url
+      required: true,
+    },
+    coverImage: {
+      type: String, // URL to the cover image it is cloudnary url
+      required: true,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+
+//to use the "this" word the callback function should be a normal function not an arrow function
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({ id: this._id,
+        username: this.username,
+        email: this.email,
+        fullName: this.fullName
+     }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRE
+    });
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRE
+    });
+}
+
+
+export const User = mongoose.model("User", userSchema);
--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -1,8 +1,8 @@
-class ApiResponse{
-    constructor(statusCode,data,message="Success"){
-        this.statusCode=statusCode<400;
-        this.data=data;
-        this.message=message;
-        this.success=true;  
-    }
+class ApiResponse {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode < 400;
+    this.data = data;
+    this.message = message;
+    this.success = true;
+  }
 }
